fac5eb14d34e53037805a03c656e2e6082f4367c
IsuruNana <ipnanayakkara@crimson.ua.edu>
2018-06-16 20:49:35 -0500

    Added JWT

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://isuru:abc123@ds163480.mlab.com:63480/dev-connector-issa'
+    mongoURI: 'mongodb://isuru:abc123@ds163480.mlab.com:63480/dev-connector-issa',
+    secretOrKey: 'secret'
 }
-
-//
