790325a9f720db776785bd662cd78178cccdb208
IsuruNana <ipnanayakkara@crimson.ua.edu>
2018-07-12 21:51:10 -0500

    Adding profile form fields

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+
 import TextFieldGroup from '../common/TextFieldGroup';
+import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
+import InputGroup from '../common/InputGroup';
+import SelectListGroup from '../common/SelectListGroup';
  
 class CreateProfile extends Component {
   constructor(props) {
@@ -23,9 +27,32 @@ class CreateProfile extends Component {
       instagram:'',
       errors:{}
     }
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+  }
+
+  onChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
   }
 
   render() {
+    const { errors } = this.state;
+
+    //Select options for status
+    const options = [
+      { label: '* Select Professional Status', value: 0 },
+      { label: 'Developer', value: 'Developer' },
+      { label: 'Manager', value: 'Manager' },
+      { label: 'Intern', value: 'Intern' }
+    ];
+
     return (
       <div className="create-profile">
         <div className="container">
@@ -36,6 +63,24 @@ class CreateProfile extends Component {
                 Lets get some information to make your profile stand out
               </p>
               <small className="d-block pb-3">* = required</small>
+              <form onSubmit={this.onSubmit}>
+                <TextFieldGroup 
+                  placeholder="* Profile Handle"
+                  name="handle"
+                  value={this.state.handle}
+                  onChange={this.onChange}
+                  error={errors.handle}
+                  info="A unique handle for your profile URL." />
+              
+                <SelectListGroup 
+                  placeholder="Status"
+                  name="status"
+                  value={this.state.status}
+                  onChange={this.onChange}
+                  error={errors.status}
+                  options={options}
+                  info="Give us an idea of where you are at in your career." />
+              </form>
             </div>
           </div>
         </div>
