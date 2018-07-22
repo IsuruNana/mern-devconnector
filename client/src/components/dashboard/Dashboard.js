32b9cbfc9c806775493f77db84a6cd199f2257fc
IsuruNana <ipnanayakkara@crimson.ua.edu>
2018-07-21 20:42:51 -0500

    Dashboard setup complete

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -6,6 +6,8 @@ import Spinner from '../common/Spinner';
 import {Link} from 'react-router-dom'
 
 import ProfileActions from './ProfileActions';
+import Experience from './Experience';
+import Education from './Education';
 
 class Dashboard extends Component {
   componentDidMount(){
@@ -32,7 +34,8 @@ class Dashboard extends Component {
           <div>
             <p className="lead text-muted">Welcome <Link to={`./profile/${profile.handle}`}>{user.name}</Link></p>
             <ProfileActions />
-            {/*TODO exp and edu*/}
+            <Experience experience={profile.experience}/>
+            <Education education={profile.education}/>
             <div style={{ marginBottom: '60px' }} />
             <button onClick={this.OnDeleteClick} className="btn btn-danger">Delete My Account</button>
           </div>
